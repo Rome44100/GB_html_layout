6293aab7980737960b3fc20851ee0e819df9f270
Rome44100 <rome44100@gmail.com>
2020-12-08 21:12:01 +0300

    Add product hover, rewrite social block, fixed errors, make button hovers and more.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ function ShowElems(ElemClick_selector, HiddenBlock_selector, opacity) {
         if('visible' != _elem.style.visibility){
             _elem.style.opacity = opacity;
             _elem.style.visibility = 'visible';
+            if('.button_menu' == ElemClick_selector && 
+                'visible' == document.querySelector('.access_form_wrap').style.visibility) {
+                document.querySelector('.access_form_wrap').style.opacity = 0;
+                document.querySelector('.access_form_wrap').style.visibility = 'hidden';
+                document.querySelector('.banner_bg').style.opacity = 0;
+                document.querySelector('.banner_bg').style.visibility = 'hidden';
+            }
+            if('.header__account' == ElemClick_selector && 
+                'visible' == document.querySelector('.main_menu').style.visibility) {
+                document.querySelector('.main_menu').style.opacity = 0;
+                document.querySelector('.main_menu').style.visibility = 'hidden';
+                document.querySelector('.banner_bg').style.opacity = 0;
+                document.querySelector('.banner_bg').style.visibility = 'hidden';
+            }
         } else {
             _elem.style.opacity = 0;
             _elem.style.visibility = 'hidden';
@@ -14,4 +28,6 @@ window.addEventListener('load', function() {
     ShowElems('#search_display', '#search_elems', 1);
     ShowElems('.button_menu', '.main_menu', 1);
     ShowElems('.button_menu', '.banner_bg', 1);
+    ShowElems('.header__account', '.access_form_wrap', 1);
+    ShowElems('.header__account', '.banner_bg', 1);
 });
